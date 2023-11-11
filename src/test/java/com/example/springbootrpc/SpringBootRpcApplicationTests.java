186ffced07b161241e20a8a2025eb34be6b9d893
Nurudeen Abdulmalik <abdulmaliknurudeen4@gmail.com>
2023-11-11 17:26:20 +0100

    first commit

---
+++ src/test/java/com/example/springbootrpc/SpringBootRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
